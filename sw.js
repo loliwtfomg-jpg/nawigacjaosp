f486196a5d009066c246aa952b008f49d18762ac
Rafał Kukla <loliwtf.omg@gmail.com>
2026-02-22 10:51:42 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='osp-lazy-admin-fixed-v1';
+const CACHE='osp-lazy-v7-contacts-custom';
 const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim();});
@@ -12,9 +12,7 @@ self.addEventListener('fetch',e=>{
   const url=new URL(req.url);
   if(url.origin===location.origin){
     e.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(req,copy));
-      return resp;
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return resp;
     }).catch(()=>cached)));
   }
 });
